92d156b3f2c1fc9e43ba13b119885084dd1edf29
K Anush Mallya <anushmallya.k@gmail.com>
2025-11-28 23:19:20 +0530

    datawarehouse_creation.sql

---
+++ scripts/datawarehouse_creation.sql
@@ -0,0 +1,9 @@
+--------------------------------------------
+-- Creating a DataBase 'Data Warehouse'
+--------------------------------------------
+'''
+Script Purpose:
+  This Script creates a new database named ''DataWarehouse'' 
+''' 
+
+CREATE DATABASE DateWarehouse;
