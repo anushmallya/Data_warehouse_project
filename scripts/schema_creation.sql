47bacc7c5988ea8e531f1e490d542146ff29933e
K Anush Mallya <anushmallya.k@gmail.com>
2025-11-28 23:23:07 +0530

    schema_creation.sql

---
+++ scripts/schema_creation.sql
@@ -0,0 +1,12 @@
+------------------------------------------
+-- Create Schemas 'Bronze,Silver,Gold'
+------------------------------------------
+'''
+Script Purpose:
+  This script sets up three schemas within the database: 'bronze','silver','gold'.
+'''
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
